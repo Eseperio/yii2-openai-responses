caeff9da9cbbeac70e90673abae1b082e7e03e97
E.Alamo <erosdelalamo@gmail.com>
2025-08-28 14:15:26 +0200

    Recover missing model
    Signed-off-by: E.Alamo <erosdelalamo@gmail.com>

---
+++ src/models/OpenAiModel.php
@@ -0,0 +1,10 @@
+<?php
+namespace eseperio\openai\responses\models;
+
+enum OpenAiModel: string
+{
+    case GPT_4_1 = 'gpt-4.1';
+    case GPT_4_1_MINI = 'gpt-4.1-mini';
+    case GPT_4O = 'gpt-4o';
+    case GPT_4O_MINI = 'gpt-4o-mini';
+}
